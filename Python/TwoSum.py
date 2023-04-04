ddf9c88beadff1fe0e8dd4f938c2cdfc71adfcc5
Vishal Maurya <vshlmaurya56@gmail.com>
2023-04-04 23:17:17 +0530

    Two Sum Solution

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -10,3 +10,15 @@ You can return the answer in any order.
 
 """
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result={}
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if (nums[i] + nums[j]) == target : 
+                    return(i,j)
+            else:
+                pass
+
+
